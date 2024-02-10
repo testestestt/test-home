306e94d81371e2c21d77ccee17c0e810fe64bdeb
Bruno Perez <konobabuno@gmail.com>
2024-02-10 17:37:49 -0600

    socials fix

--- js/home.js
+++ js/home.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
     headerHeight = $('header').height();
-    console.log(headerHeight);
-    $('.view-height').css('height', 'calc(100vh - ' + headerHeight + 'px)');
+    innerHeight = window.innerHeight;
+    console.log(innerHeight);
+    $('.view-height').css('height', innerHeight -  headerHeight);
 });
 
 $(window).resize(function() {
