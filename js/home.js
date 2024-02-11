c682f10564e4b05e962b5e0358e3fbce34ccfb08
Bruno Perez <konobabuno@gmail.com>
2024-02-10 18:49:02 -0600

    height resize

--- js/home.js
+++ js/home.js
@@ -1,12 +1,12 @@
 $(window).resize(function() {
     headerHeight = $('header').height();
     innerHeightVar = window.innerHeight;
-    $('.view-height').css('height', innerHeightVar -  headerHeight);
+    $('.view-height').css('height', 'calc(100vh - ' + headerHeight + 'px)');
 });
 
 $(document).ready(function() {
     headerHeight = $('header').height();
     innerHeightVar = window.innerHeight;
     $('.view-height').css('height', innerHeight -  headerHeight);
-    $('.socials').css('transform', 'translateY('+ (innerHeightVar - $('.socials').height() - 15) + 'px)');
+    $('.socials').css('transform', 'translateY('+ (innerHeightVar - $('.socials').height() - 10) + 'px)');
 });
